62e4e820d66ba7cd8d11713f2dcd1afe9df478fe
mikejeter <mjeter45@yahoo.com>
2019-08-10 13:42:18 -0400

    modified function-conversion.js, objects.js

--- assignments/objects.js
+++ assignments/objects.js
@@ -19,6 +19,14 @@ const example = {
 
 // Write your intern objects here:
 
+const intern = {
+  "id": 0,
+  "name": "Mitzi",
+  "email": "mmelloy0@psu.edu",
+  "gender": "F",
+  
+}
+
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
